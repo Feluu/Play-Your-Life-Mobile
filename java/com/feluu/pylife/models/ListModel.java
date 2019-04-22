a784ec7648d44055cdccd613906e7d7392711a53
Feluu <rysiak22@gmail.com>
2019-04-23 01:22:54 +0200

    Create ListModel.java

---
+++ java/com/feluu/pylife/models/ListModel.java
@@ -0,0 +1,44 @@
+package com.feluu.pylife.models;
+
+public class ListModel {
+
+    private int name;
+    private int first;
+    private int second;
+    private int third;
+    private int fourth;
+    private int image;
+
+    public ListModel(int name, int first, int second, int third, int fourth, int image) {
+        this.name = name;
+        this.first = first;
+        this.second = second;
+        this.third = third;
+        this.fourth = fourth;
+        this.image = image;
+    }
+
+    public int getName() {
+        return name;
+    }
+
+    public int getFirst() {
+        return first;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public int getThird() {
+        return third;
+    }
+
+    public int getFourth() {
+        return fourth;
+    }
+
+    public int getImage() {
+        return image;
+    }
+}
