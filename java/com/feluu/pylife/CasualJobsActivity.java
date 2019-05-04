249c9b215bb08deea8d6c0f6de4d8ee92c8120ee
Feluu <rysiak22@gmail.com>
2019-05-04 21:32:28 +0200

    Changed integers to strings

--- java/com/feluu/pylife/CasualJobsActivity.java
+++ java/com/feluu/pylife/CasualJobsActivity.java
@@ -36,34 +36,37 @@ public class CasualJobsActivity extends AppCompatActivity {
         });
 
         models = new ArrayList<>();
-        models.add(new ViewModel(R.drawable.pizzaboy, R.string.pizza_title, R.string.pizza_earnings, R.string.pizza_locations, R.string.pizza_description));
-        models.add(new ViewModel(R.drawable.sweeper, R.string.sweeper_title, R.string.sweeper_earnings, R.string.sweeper_locations, R.string.sweeper_description));
-        models.add(new ViewModel(R.drawable.h1, R.string.h1_title, R.string.h1_earnings, R.string.h1_locations, R.string.h1_description));
-        models.add(new ViewModel(R.drawable.l1, R.string.l1_title, R.string.l1_earnings, R.string.l1_locations, R.string.l1_description));
-        models.add(new ViewModel(R.drawable.maveric, R.string.h2_title, R.string.h2_earnings, R.string.h2_locations, R.string.h2_description));
-        models.add(new ViewModel(R.drawable.crop, R.string.l2_title, R.string.l2_earnings, R.string.l2_locations, R.string.l2_description));
-        models.add(new ViewModel(R.drawable.dron, R.string.l2d_title, R.string.l2d_earnings, R.string.l2d_locations, R.string.l2d_description));
-        models.add(new ViewModel(R.drawable.dune, R.string.dune_title, R.string.dune_earnings, R.string.dune_locations, R.string.dune_description));
-        models.add(new ViewModel(R.drawable.smieciarka, R.string.zgk_title, R.string.zgk_earnings, R.string.zgk_locations, R.string.zgk_description));
-        models.add(new ViewModel(R.drawable.lowisko, R.string.wed_title, R.string.wed_earnings, R.string.wed_locations, R.string.wed_description));
-        models.add(new ViewModel(R.drawable.bus, R.string.bus_title, R.string.bus_earnings, R.string.bus_locations, R.string.bus_description));
-        models.add(new ViewModel(R.drawable.hol, R.string.hol_title, R.string.hol_earnings, R.string.hol_locations, R.string.hol_description));
-        models.add(new ViewModel(R.drawable.way, R.string.kata_title, R.string.kata_earnings, R.string.kata_locations, R.string.kata_description));
-        models.add(new ViewModel(R.drawable.aparat, R.string.photo_title, R.string.photo_earnings, R.string.photo_locations, R.string.photo_description));
-        models.add(new ViewModel(R.drawable.kutr, R.string.ryb_title, R.string.ryb_earnings, R.string.ryb_locations, R.string.ryb_description));
-        models.add(new ViewModel(R.drawable.kurier, R.string.kurier_title, R.string.kurier_earnings, R.string.kurier_locations, R.string.kurier_description));
-        models.add(new ViewModel(R.drawable.nurek, R.string.nurek_title, R.string.nurek_earnings, R.string.nurek_locations, R.string.nurek_description));
-        models.add(new ViewModel(R.drawable.sv, R.string.sv_title, R.string.sv_earnings, R.string.sv_locations, R.string.sv_description));
-        models.add(new ViewModel(R.drawable.bet, R.string.bet_title, R.string.bet_earnings, R.string.bet_locations, R.string.bet_description));
-        models.add(new ViewModel(R.drawable.zlodziej, R.string.zlodziej_title, R.string.zlodziej_earnings, R.string.zlodziej_locations, R.string.zlodziej_description));
-        models.add(new ViewModel(R.drawable.taxi, R.string.taxi1_title, R.string.taxi1_earnings, R.string.taxi_locations, R.string.taxi_description));
-        models.add(new ViewModel(R.drawable.taxi, R.string.taxi2_title, R.string.taxi2_earnings, R.string.taxi_locations, R.string.taxi_description));
-        models.add(new ViewModel(R.drawable.taxi, R.string.taxi3_title, R.string.taxi3_earnings, R.string.taxi_locations, R.string.taxi_description));
-        models.add(new ViewModel(R.drawable.taxi, R.string.taxi4_title, R.string.taxi4_earnings, R.string.taxi_locations, R.string.taxi_description));
-
+        models.add(new ViewModel(R.drawable.pizzaboy, intToString(R.string.pizza_title), intToString(R.string.pizza_earnings), intToString(R.string.pizza_locations), intToString(R.string.pizza_description)));
+        models.add(new ViewModel(R.drawable.sweeper, intToString(R.string.sweeper_title), intToString(R.string.sweeper_earnings), intToString(R.string.sweeper_locations), intToString(R.string.sweeper_description)));
+        models.add(new ViewModel(R.drawable.h1, intToString(R.string.h1_title), intToString(R.string.h1_earnings), intToString(R.string.h1_locations), intToString(R.string.h1_description)));
+        models.add(new ViewModel(R.drawable.l1, intToString(R.string.l1_title), intToString(R.string.l1_earnings), intToString(R.string.l1_locations), intToString(R.string.l1_description)));
+        models.add(new ViewModel(R.drawable.maveric, intToString(R.string.h2_title), intToString(R.string.h2_earnings), intToString(R.string.h2_locations), intToString(R.string.h2_description)));
+        models.add(new ViewModel(R.drawable.crop, intToString(R.string.l2_title), intToString(R.string.l2_earnings), intToString(R.string.l2_locations), intToString(R.string.l2_description)));
+        models.add(new ViewModel(R.drawable.dron, intToString(R.string.l2d_title), intToString(R.string.l2d_earnings), intToString(R.string.l2d_locations), intToString(R.string.l2d_description)));
+        models.add(new ViewModel(R.drawable.dune, intToString(R.string.dune_title), intToString(R.string.dune_earnings), intToString(R.string.dune_locations), intToString(R.string.dune_description)));
+        models.add(new ViewModel(R.drawable.smieciarka, intToString(R.string.zgk_title), intToString(R.string.zgk_earnings), intToString(R.string.zgk_locations), intToString(R.string.zgk_description)));
+        models.add(new ViewModel(R.drawable.lowisko, intToString(R.string.wed_title), intToString(R.string.wed_earnings), intToString(R.string.wed_locations), intToString(R.string.wed_description)));
+        models.add(new ViewModel(R.drawable.bus, intToString(R.string.bus_title), intToString(R.string.bus_earnings), intToString(R.string.bus_locations), intToString(R.string.bus_description)));
+        models.add(new ViewModel(R.drawable.hol, intToString(R.string.hol_title), intToString(R.string.hol_earnings), intToString(R.string.hol_locations), intToString(R.string.hol_description)));
+        models.add(new ViewModel(R.drawable.way, intToString(R.string.kata_title), intToString(R.string.kata_earnings), intToString(R.string.kata_locations), intToString(R.string.kata_description)));
+        models.add(new ViewModel(R.drawable.aparat, intToString(R.string.photo_title), intToString(R.string.photo_earnings), intToString(R.string.photo_locations), intToString(R.string.photo_description)));
+        models.add(new ViewModel(R.drawable.kutr, intToString(R.string.ryb_title), intToString(R.string.ryb_earnings), intToString(R.string.ryb_locations), intToString(R.string.ryb_description)));
+        models.add(new ViewModel(R.drawable.kurier, intToString(R.string.kurier_title), intToString(R.string.kurier_earnings), intToString(R.string.kurier_locations), intToString(R.string.kurier_description)));
+        models.add(new ViewModel(R.drawable.nurek, intToString(R.string.nurek_title), intToString(R.string.nurek_earnings), intToString(R.string.nurek_locations), intToString(R.string.nurek_description)));
+        models.add(new ViewModel(R.drawable.sv, intToString(R.string.sv_title), intToString(R.string.sv_earnings), intToString(R.string.sv_locations), intToString(R.string.sv_description)));
+        models.add(new ViewModel(R.drawable.bet, intToString(R.string.bet_title), intToString(R.string.bet_earnings), intToString(R.string.bet_locations), intToString(R.string.bet_description)));
+        models.add(new ViewModel(R.drawable.zlodziej, intToString(R.string.zlodziej_title), intToString(R.string.zlodziej_earnings), intToString(R.string.zlodziej_locations), intToString(R.string.zlodziej_description)));
+        models.add(new ViewModel(R.drawable.taxi, intToString(R.string.taxi1_title), intToString(R.string.taxi1_earnings), intToString(R.string.taxi_locations), intToString(R.string.taxi_description)));
+        models.add(new ViewModel(R.drawable.taxi, intToString(R.string.taxi2_title), intToString(R.string.taxi2_earnings), intToString(R.string.taxi_locations), intToString(R.string.taxi_description)));
+        models.add(new ViewModel(R.drawable.taxi, intToString(R.string.taxi3_title), intToString(R.string.taxi3_earnings), intToString(R.string.taxi_locations), intToString(R.string.taxi_description)));
+        models.add(new ViewModel(R.drawable.taxi, intToString(R.string.taxi4_title), intToString(R.string.taxi4_earnings), intToString(R.string.taxi_locations), intToString(R.string.taxi_description)));
         adapter = new JobsAdapter(models, this);
 
         viewPager.setAdapter(adapter);
         viewPager.setPadding(130, 0, 130, 0);
+    }
+
+    public String intToString(int Res) {
+        return getResources().getString(Res);
     }
 }
