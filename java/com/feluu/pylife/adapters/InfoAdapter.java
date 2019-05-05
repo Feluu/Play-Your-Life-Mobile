ad65fc1d93b475aa734f065979ce4525ecbdda52
Feluu <rysiak22@gmail.com>
2019-05-05 13:02:56 +0200

    Improved view create

--- java/com/feluu/pylife/adapters/InfoAdapter.java
+++ java/com/feluu/pylife/adapters/InfoAdapter.java
@@ -14,8 +14,8 @@ import java.util.ArrayList;
 
 public class InfoAdapter extends ArrayAdapter<ListModel> {
 
-    ArrayList<ListModel> dataSet;
-    Context mContext;
+    private ArrayList<ListModel> dataSet;
+    private Context mContext;
 
     private static class ViewHolder {
         private TextView firstText, secondText;
@@ -28,16 +28,22 @@ public class InfoAdapter extends ArrayAdapter<ListModel> {
 
     }
 
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        ListModel adapter = getItem(position);
-        ViewHolder viewHolder = new ViewHolder();
-        LayoutInflater inflater = LayoutInflater.from(getContext());
-        convertView = inflater.inflate(R.layout.info_layout, parent, false);
-        viewHolder.firstText = convertView.findViewById(R.id.firstText);
-        viewHolder.secondText = convertView.findViewById(R.id.secondText);
-        viewHolder.firstText.setText(adapter.getName());
-        viewHolder.secondText.setText(adapter.getFirst());
-        return convertView;
-    }
+   @Override
+   public View getView(int position, View convertView, ViewGroup parent) {
+       ListModel adapter = getItem(position);
+       ViewHolder viewHolder;
+       if (convertView == null) {
+           viewHolder = new ViewHolder();
+           LayoutInflater inflater = LayoutInflater.from(getContext());
+           convertView = inflater.inflate(R.layout.info_layout, parent, false);
+           viewHolder.firstText = convertView.findViewById(R.id.firstText);
+           viewHolder.secondText = convertView.findViewById(R.id.secondText);
+           convertView.setTag(viewHolder);
+       } else {
+           viewHolder = (ViewHolder) convertView.getTag();
+       }
+       viewHolder.firstText.setText(adapter.getName());
+       viewHolder.secondText.setText(adapter.getFirst());
+       return convertView;
+   }
 }
