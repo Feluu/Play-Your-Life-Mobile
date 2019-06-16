16cb15c49700a25eb26df2dc766f22c7af98da7e
Feluu <rysiak22@gmail.com>
2019-06-16 13:25:56 +0200

    Use prefs for dark theme and wheels

---
+++ java/com/feluu/pylife/utils/SharedPref.java
@@ -0,0 +1,34 @@
+package com.feluu.pylife.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPref {
+    SharedPreferences mySharedPref;
+
+    public SharedPref(Context context) {
+        mySharedPref = context.getSharedPreferences("settings", Context.MODE_PRIVATE);
+    }
+
+    public void setNightModeState(Boolean state) {
+        SharedPreferences.Editor editor = mySharedPref.edit();
+        editor.putBoolean("NightMode", state);
+        editor.commit();
+    }
+
+    public void setAvailableWheels(String wheels) {
+        SharedPreferences.Editor editor = mySharedPref.edit();
+        editor.putString("Wheels", wheels);
+        editor.commit();
+    }
+
+    public Boolean loadNightModeState() {
+        Boolean state = mySharedPref.getBoolean("NightMode", false);
+        return state;
+    }
+
+    public String loadAvailableWheels() {
+        String wheels = mySharedPref.getString("Wheels", null);
+        return wheels;
+    }
+}
