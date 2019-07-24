dbc61741141c6d87b871bfe0d28bee3d9b9b17b9
Feluu <rysiak22@gmail.com>
2019-07-24 14:54:53 +0200

    Implemented search function

--- java/com/feluu/pylife/adapters/MechanicalAdapter.java
+++ java/com/feluu/pylife/adapters/MechanicalAdapter.java
@@ -1,10 +1,14 @@
 package com.feluu.pylife.adapters;
 
 import android.content.Context;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,27 +17,31 @@ import com.bumptech.glide.request.RequestOptions;
 import com.feluu.pylife.R;
 import com.feluu.pylife.models.ListModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MechanicalAdapter extends RecyclerView.Adapter<MechanicalAdapter.MechanicalViewHolder> {
+public class MechanicalAdapter extends RecyclerView.Adapter<MechanicalAdapter.MechanicalViewHolder> implements Filterable {
 
     private Context mCtx;
     private List<ListModel> mechanicalList;
+    private List<ListModel> mechanicalListFull;
 
     public MechanicalAdapter(Context mCtx, List<ListModel> mechanicalList) {
         this.mCtx = mCtx;
         this.mechanicalList = mechanicalList;
+        mechanicalListFull = new ArrayList<>(mechanicalList);
     }
 
+    @NonNull
     @Override
-    public MechanicalViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public MechanicalViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(mCtx);
-        View view = inflater.inflate(R.layout.mechanical_layout, null);
+        View view = inflater.inflate(R.layout.mechanical_layout, parent, false);
         return new MechanicalViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(MechanicalViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MechanicalViewHolder holder, int position) {
         ListModel mechanical = mechanicalList.get(position);
 
         holder.textViewName.setText(mechanical.getName());
@@ -54,12 +62,44 @@ public class MechanicalAdapter extends RecyclerView.Adapter<MechanicalAdapter.Me
         return mechanicalList.size();
     }
 
+    @Override
+    public Filter getFilter() {
+        return mechanicalFilter;
+    }
+
+    private Filter mechanicalFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<ListModel> filteredList = new ArrayList<>();
+            if (constraint == null || constraint.length() == 0) {
+                filteredList.addAll(mechanicalListFull);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for (ListModel model : mechanicalListFull) {
+                    if (model.getName().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(model);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mechanicalList.clear();
+            mechanicalList.addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+    };
+
     class MechanicalViewHolder extends RecyclerView.ViewHolder {
 
         private TextView textViewName, textViewAllows, textViewPrice, textViewReturn;
         private ImageView imageView2;
 
-        public MechanicalViewHolder(View itemView) {
+        MechanicalViewHolder(View itemView) {
             super(itemView);
 
             textViewName = itemView.findViewById(R.id.wheelsName);
