fd73433ccb500ab31b568e56fa088dd47fae01d1
Feluu <rysiak22@gmail.com>
2019-07-24 15:01:00 +0200

    Code improvements

--- java/com/feluu/pylife/SpoilersTuneActivity.java
+++ java/com/feluu/pylife/SpoilersTuneActivity.java
@@ -10,7 +10,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.feluu.pylife.adapters.WheelsAdapter;
+import com.feluu.pylife.adapters.SpoilersAdapter;
 import com.feluu.pylife.models.ListModel;
 import com.feluu.pylife.utils.SharedPref;
 
@@ -28,7 +28,7 @@ public class SpoilersTuneActivity extends AppCompatActivity {
             setTheme(R.style.AppTheme);
         }
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list);
+        setContentView(R.layout.activity_list_search);
 
         ImageView leaveActivity;
         ImageView bgMain;
@@ -41,12 +41,7 @@ public class SpoilersTuneActivity extends AppCompatActivity {
         RecyclerView recyclerView;
 
         activityTitle.setText(R.string.spoilers_list_activity);
-        leaveActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        leaveActivity.setOnClickListener((View v) -> finish());
 
         recyclerView = findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
@@ -78,7 +73,7 @@ public class SpoilersTuneActivity extends AppCompatActivity {
         spoilersList.add(new ListModel(intToString(R.string.spoiler_carbon_name), intToString(R.string.spoiler_carbon_price), intToString(R.string.spoiler_carbon_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.spoiler_carbon));
         spoilersList.add(new ListModel(intToString(R.string.spoiler_alienvi_name), intToString(R.string.spoiler_alienvi_price), intToString(R.string.spoiler_alienvi_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.spoiler_alien_vi));
 
-        WheelsAdapter adapter = new WheelsAdapter(this, spoilersList);
+        SpoilersAdapter adapter = new SpoilersAdapter(this, spoilersList);
         recyclerView.setAdapter(adapter);
     }
 
