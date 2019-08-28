be054b11b81d60b5934742f5a67262697ddb9193
Feluu <rysiak22@gmail.com>
2019-08-28 14:31:33 +0200

    Optimizations

--- java/com/feluu/pylife/CountersTuneActivity.java
+++ java/com/feluu/pylife/CountersTuneActivity.java
@@ -35,7 +35,7 @@ public class CountersTuneActivity extends AppCompatActivity {
 
         leaveActivity = findViewById(R.id.exitActivity);
         bgMain = findViewById(R.id.bgmain);
-        activityTitle = findViewById(R.id.textView1);
+        activityTitle = findViewById(R.id.top_title);
 
         List<ListModel> countersList;
         RecyclerView recyclerView;
@@ -55,10 +55,10 @@ public class CountersTuneActivity extends AppCompatActivity {
 
         countersList = new ArrayList<>();
 
-        countersList.add(new ListModel(intToString(R.string.standard_counter_name), intToString(R.string.standard_counter_price), intToString(R.string.standard_counter_range), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.standard_licznik));
-        countersList.add(new ListModel(intToString(R.string.neo_name), intToString(R.string.neo_price), intToString(R.string.neo_range), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.neo));
-        countersList.add(new ListModel(intToString(R.string.one_name), intToString(R.string.one_price), intToString(R.string.one_range), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.one));
-        countersList.add(new ListModel(intToString(R.string.style_name), intToString(R.string.style_price), intToString(R.string.style_range), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.style));
+        countersList.add(new ListModel(intToString(R.string.standard_counter_name), intToString(R.string.standard_counter_price), intToString(R.string.standard_counter_range), null, null, R.drawable.standard_licznik));
+        countersList.add(new ListModel(intToString(R.string.neo_name), intToString(R.string.neo_price), intToString(R.string.neo_range), null, null, R.drawable.neo));
+        countersList.add(new ListModel(intToString(R.string.one_name), intToString(R.string.one_price), intToString(R.string.one_range), null, null, R.drawable.one));
+        countersList.add(new ListModel(intToString(R.string.style_name), intToString(R.string.style_price), intToString(R.string.style_range), null, null, R.drawable.style));
 
         CountersAdapter adapter = new CountersAdapter(this, countersList);
         recyclerView.setAdapter(adapter);
