6fbe2093ce3efb738fc2055b05f90b2be9ad1179
Feluu <rysiak22@gmail.com>
2019-08-28 14:38:07 +0200

    Improvements

--- java/com/feluu/pylife/models/ListModel.java
+++ java/com/feluu/pylife/models/ListModel.java
@@ -2,16 +2,12 @@ package com.feluu.pylife.models;
 
 public class ListModel {
 
-    private String name;
     private String first;
     private String second;
     private String third;
     private String fourth;
+    private String fifth;
     private int image;
-
-    public String getName() {
-        return name;
-    }
 
     public String getFirst() {
         return first;
@@ -29,16 +25,20 @@ public class ListModel {
         return fourth;
     }
 
+    public String getFifth() {
+        return fifth;
+    }
+
     public int getImage() {
         return image;
     }
 
-    public ListModel(String name, String first, String second, String third, String fourth, int image) {
-        this.name = name;
+    public ListModel(String first, String second, String third, String fourth, String fifth, int image) {
         this.first = first;
         this.second = second;
         this.third = third;
         this.fourth = fourth;
+        this.fifth = fifth;
         this.image = image;
     }
 
