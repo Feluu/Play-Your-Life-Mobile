8695ba95639824459db63c8c555e19647ace0fdf
Feluu <rysiak22@gmail.com>
2019-08-28 14:34:25 +0200

    Optimizations

--- java/com/feluu/pylife/WheelsTuneActivity.java
+++ java/com/feluu/pylife/WheelsTuneActivity.java
@@ -1,6 +1,5 @@
 package com.feluu.pylife;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -8,23 +7,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.viewpager.widget.ViewPager;
 
+import com.feluu.pylife.adapters.AvWheelsAdapter;
 import com.feluu.pylife.adapters.WheelsAdapter;
 import com.feluu.pylife.models.ListModel;
+import com.feluu.pylife.models.ViewModel;
 import com.feluu.pylife.utils.SharedPref;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.relex.circleindicator.CircleIndicator;
+
 public class WheelsTuneActivity extends AppCompatActivity {
+
+    public RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,25 +42,13 @@ public class WheelsTuneActivity extends AppCompatActivity {
         ImageView bgMain;
         TextView activityTitle;
 
+        recyclerView = findViewById(R.id.recyclerView);
         leaveActivity = findViewById(R.id.exitActivity);
         bgMain = findViewById(R.id.bgmain);
-        activityTitle = findViewById(R.id.textView1);
-        List<ListModel> wheelsList;
-        RecyclerView recyclerView;
-        final SwipeRefreshLayout swipeRefreshLayout;
+        activityTitle = findViewById(R.id.top_title);
 
         activityTitle.setText(R.string.wheels_list_activity);
         leaveActivity.setOnClickListener((View v) -> finish());
-
-        swipeRefreshLayout = findViewById(R.id.swipeRefresh);
-        swipeRefreshLayout.setOnRefreshListener(() -> {
-            getWheels();
-            swipeRefreshLayout.setRefreshing(false);
-        });
-
-        recyclerView = findViewById(R.id.recyclerView);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         if (sharedPref.loadNightModeState()) {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
@@ -67,73 +56,114 @@ public class WheelsTuneActivity extends AppCompatActivity {
             bgMain.setImageResource(R.drawable.bg_dark);
         }
 
+        setWheels();
+        setAvailableWheels();
+    }
+
+    private void setWheels(){
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        List<ListModel> wheelsList;
         wheelsList = new ArrayList<>();
-
-        wheelsList.add(new ListModel(intToString(R.string.ahab_name), intToString(R.string.ahab_price), intToString(R.string.ahab_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.ahab));
-        wheelsList.add(new ListModel(intToString(R.string.classic_name), intToString(R.string.classic_price), intToString(R.string.classic_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.classic));
-        wheelsList.add(new ListModel(intToString(R.string.cutter_name), intToString(R.string.cutter_price), intToString(R.string.cutter_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.cutter));
-        wheelsList.add(new ListModel(intToString(R.string.dollar_name), intToString(R.string.dollar_price), intToString(R.string.dollar_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.dollar));
-        wheelsList.add(new ListModel(intToString(R.string.grove_name), intToString(R.string.grove_price), intToString(R.string.grove_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.grove));
-        wheelsList.add(new ListModel(intToString(R.string.import_name), intToString(R.string.import_price), intToString(R.string.import_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.importy));
-        wheelsList.add(new ListModel(intToString(R.string.mega_name), intToString(R.string.mega_price), intToString(R.string.mega_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.mega));
-        wheelsList.add(new ListModel(intToString(R.string.mesh_name), intToString(R.string.mesh_price), intToString(R.string.mesh_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.mesh));
-        wheelsList.add(new ListModel(intToString(R.string.offroad_name), intToString(R.string.offroad_price), intToString(R.string.offroad_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.offroad));
-        wheelsList.add(new ListModel(intToString(R.string.rimshine_name), intToString(R.string.rimshine_price), intToString(R.string.rimshine_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.rimshine));
-        wheelsList.add(new ListModel(intToString(R.string.rittle_name), intToString(R.string.rittle_price), intToString(R.string.rittle_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.rittle));
-        wheelsList.add(new ListModel(intToString(R.string.stalowe_name), intToString(R.string.stalowe_price), intToString(R.string.stalowe_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.stalowe));
-        wheelsList.add(new ListModel(intToString(R.string.switch_name), intToString(R.string.switch_price), intToString(R.string.switch_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.switche));
-        wheelsList.add(new ListModel(intToString(R.string.timer_name), intToString(R.string.timer_price), intToString(R.string.timer_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.timer));
-        wheelsList.add(new ListModel(intToString(R.string.twist_name), intToString(R.string.twist_price), intToString(R.string.twist_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.twist));
-        wheelsList.add(new ListModel(intToString(R.string.virtual_name), intToString(R.string.virtual_price), intToString(R.string.virtual_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.virtual));
-        wheelsList.add(new ListModel(intToString(R.string.wires_name), intToString(R.string.wires_price), intToString(R.string.wires_demont), intToString(R.string.layout_null), intToString(R.string.layout_null), R.drawable.wires));
-
+        wheelsList.add(new ListModel(intToString(R.string.ahab_name), intToString(R.string.ahab_price), intToString(R.string.ahab_demont), null, null, R.drawable.ahab));
+        wheelsList.add(new ListModel(intToString(R.string.classic_name), intToString(R.string.classic_price), intToString(R.string.classic_demont), null, null, R.drawable.classic));
+        wheelsList.add(new ListModel(intToString(R.string.cutter_name), intToString(R.string.cutter_price), intToString(R.string.cutter_demont), null, null, R.drawable.cutter));
+        wheelsList.add(new ListModel(intToString(R.string.dollar_name), intToString(R.string.dollar_price), intToString(R.string.dollar_demont), null, null, R.drawable.dollar));
+        wheelsList.add(new ListModel(intToString(R.string.grove_name), intToString(R.string.grove_price), intToString(R.string.grove_demont), null, null, R.drawable.grove));
+        wheelsList.add(new ListModel(intToString(R.string.import_name), intToString(R.string.import_price), intToString(R.string.import_demont), null, null, R.drawable.importy));
+        wheelsList.add(new ListModel(intToString(R.string.mega_name), intToString(R.string.mega_price), intToString(R.string.mega_demont), null, null, R.drawable.mega));
+        wheelsList.add(new ListModel(intToString(R.string.mesh_name), intToString(R.string.mesh_price), intToString(R.string.mesh_demont), null, null, R.drawable.mesh));
+        wheelsList.add(new ListModel(intToString(R.string.offroad_name), intToString(R.string.offroad_price), intToString(R.string.offroad_demont), null, null, R.drawable.offroad));
+        wheelsList.add(new ListModel(intToString(R.string.rimshine_name), intToString(R.string.rimshine_price), intToString(R.string.rimshine_demont), null, null, R.drawable.rimshine));
+        wheelsList.add(new ListModel(intToString(R.string.rittle_name), intToString(R.string.rittle_price), intToString(R.string.rittle_demont), null, null, R.drawable.rittle));
+        wheelsList.add(new ListModel(intToString(R.string.stalowe_name), intToString(R.string.stalowe_price), intToString(R.string.stalowe_demont), null, null, R.drawable.stalowe));
+        wheelsList.add(new ListModel(intToString(R.string.switch_name), intToString(R.string.switch_price), intToString(R.string.switch_demont), null, null, R.drawable.switche));
+        wheelsList.add(new ListModel(intToString(R.string.timer_name), intToString(R.string.timer_price), intToString(R.string.timer_demont), null, null, R.drawable.timer));
+        wheelsList.add(new ListModel(intToString(R.string.twist_name), intToString(R.string.twist_price), intToString(R.string.twist_demont), null, null, R.drawable.twist));
+        wheelsList.add(new ListModel(intToString(R.string.virtual_name), intToString(R.string.virtual_price), intToString(R.string.virtual_demont), null, null, R.drawable.virtual));
+        wheelsList.add(new ListModel(intToString(R.string.wires_name), intToString(R.string.wires_price), intToString(R.string.wires_demont), null, null, R.drawable.wires));
         WheelsAdapter adapter = new WheelsAdapter(this, wheelsList);
         recyclerView.setAdapter(adapter);
-
-        getWheels();
     }
 
-    public String intToString(int Res) {
+    private void setAvailableWheels(){
+        Bundle extras = getIntent().getExtras();
+        String avWheels = extras.getString("avWheels").toLowerCase();
+        NestedScrollView nestedScrollView;
+        ViewPager viewPager;
+        CircleIndicator indicator;
+        nestedScrollView = findViewById(R.id.nestedScrollView);
+        viewPager = findViewById(R.id.viewPager);
+        indicator = findViewById(R.id.indicator);
+
+        if (avWheels.equalsIgnoreCase("error")) {
+            viewPager.setVisibility(View.GONE);
+            indicator.setVisibility(View.GONE);
+            nestedScrollView.getParent().requestChildFocus(nestedScrollView, nestedScrollView);
+        } else {
+            List<ViewModel> models;
+            models = new ArrayList<>();
+            if (avWheels.contains("ahab")) {
+                models.add(new ViewModel(R.drawable.ahab, intToString(R.string.ahab_name), intToString(R.string.ahab_price), intToString(R.string.ahab_demont), null));
+            }
+            if (avWheels.contains("classic")) {
+                models.add(new ViewModel(R.drawable.classic, intToString(R.string.classic_name), intToString(R.string.classic_price), intToString(R.string.classic_demont), null));
+            }
+            if (avWheels.contains("cutter")) {
+                models.add(new ViewModel(R.drawable.cutter, intToString(R.string.cutter_name), intToString(R.string.cutter_price), intToString(R.string.cutter_demont), null));
+            }
+            if (avWheels.contains("dollar")) {
+                models.add(new ViewModel(R.drawable.dollar, intToString(R.string.dollar_name), intToString(R.string.dollar_price), intToString(R.string.dollar_demont), null));
+            }
+            if (avWheels.contains("grove")) {
+                models.add(new ViewModel(R.drawable.grove, intToString(R.string.grove_name), intToString(R.string.grove_price), intToString(R.string.grove_demont), null));
+            }
+            if (avWheels.contains("import")) {
+                models.add(new ViewModel(R.drawable.importy, intToString(R.string.import_name), intToString(R.string.import_price), intToString(R.string.import_demont), null));
+            }
+            if (avWheels.contains("mega") || (avWheels.contains("megi"))) {
+                models.add(new ViewModel(R.drawable.mega, intToString(R.string.mega_name), intToString(R.string.mega_price), intToString(R.string.mega_demont), null));
+            }
+            if (avWheels.contains("mesh")) {
+                models.add(new ViewModel(R.drawable.mesh, intToString(R.string.mesh_name), intToString(R.string.mesh_price), intToString(R.string.mesh_demont), null));
+            }
+            if (avWheels.contains("offroad") || avWheels.contains("off-road")) {
+                models.add(new ViewModel(R.drawable.offroad, intToString(R.string.offroad_name), intToString(R.string.offroad_price), intToString(R.string.offroad_demont), null));
+            }
+            if (avWheels.contains("rimshine")) {
+                models.add(new ViewModel(R.drawable.rimshine, intToString(R.string.rimshine_name), intToString(R.string.rimshine_price), intToString(R.string.rimshine_demont), null));
+            }
+            if (avWheels.contains("rittle")) {
+                models.add(new ViewModel(R.drawable.rittle, intToString(R.string.rittle_name), intToString(R.string.rittle_price), intToString(R.string.rittle_demont), null));
+            }
+            if (avWheels.contains("stalowe")) {
+                models.add(new ViewModel(R.drawable.stalowe, intToString(R.string.stalowe_name), intToString(R.string.stalowe_price), intToString(R.string.stalowe_demont), null));
+            }
+            if (avWheels.contains("switch")) {
+                models.add(new ViewModel(R.drawable.switche, intToString(R.string.switch_name), intToString(R.string.switch_price), intToString(R.string.switch_demont), null));
+            }
+            if (avWheels.contains("timer")) {
+                models.add(new ViewModel(R.drawable.timer, intToString(R.string.timer_name), intToString(R.string.timer_price), intToString(R.string.timer_demont), null));
+            }
+            if (avWheels.contains("twist")) {
+                models.add(new ViewModel(R.drawable.twist, intToString(R.string.twist_name), intToString(R.string.twist_price), intToString(R.string.twist_demont), null));
+            }
+            if (avWheels.contains("virtual")) {
+                models.add(new ViewModel(R.drawable.virtual, intToString(R.string.virtual_name), intToString(R.string.virtual_price), intToString(R.string.virtual_demont), null));
+            }
+            if (avWheels.contains("wires")) {
+                models.add(new ViewModel(R.drawable.wires, intToString(R.string.wires_name), intToString(R.string.wires_price), intToString(R.string.wires_demont), null));
+            }
+            AvWheelsAdapter adapter = new AvWheelsAdapter(models, this);
+            viewPager.setAdapter(adapter);
+            viewPager.setPadding(130, 0, 130, 0);
+            nestedScrollView.getParent().requestChildFocus(nestedScrollView, nestedScrollView);
+            indicator.setViewPager(viewPager);
+        }
+    }
+
+    private String intToString(int Res) {
         return getResources().getString(Res);
-    }
-
-    private void getWheels(){
-        try {
-            URL url = new URL("https://feluu.pl/wheels.txt");
-            new ReadWheelsTask().execute(url);
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-    }
-    private class ReadWheelsTask extends AsyncTask<URL, Void, String> {
-
-        private String str = null;
-
-        @Override
-        protected String doInBackground(URL... urls) {
-            try {
-                BufferedReader in = new BufferedReader(new InputStreamReader(urls[0].openStream()));
-                str = in.readLine();
-                in.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            SharedPref sharedPref = new SharedPref(getApplicationContext());
-            TextView wheels = findViewById(R.id.available);
-            String av = sharedPref.loadAvailableWheels();
-            if (str == null && av == null) {
-                wheels.setText(R.string.layout_cannot_retrieve_wheels);
-            } else if (str == null && av != null) {
-                wheels.setText(av);
-            } else {
-                sharedPref.setAvailableWheels(str);
-                wheels.setText(str);
-            }
-        }
     }
 }
