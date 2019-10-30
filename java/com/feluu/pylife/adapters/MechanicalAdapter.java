537a44bb387f8230ffd88ff1e81f6a3b394ce2b1
Feluu <rysiak22@gmail.com>
2019-10-30 18:37:27 +0100

    Improvements, changed deprecated functions

--- java/com/feluu/pylife/adapters/MechanicalAdapter.java
+++ java/com/feluu/pylife/adapters/MechanicalAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.feluu.pylife.R;
 import com.feluu.pylife.models.ListModel;
@@ -54,7 +56,8 @@ public class MechanicalAdapter extends RecyclerView.Adapter<MechanicalAdapter.Me
         holder.textViewReturn.setText(mechanical.getFourth());
         Glide
                 .with(mCtx)
-                .load(mCtx.getResources().getDrawable(mechanical.getImage()))
+                .load(ContextCompat.getDrawable(mCtx, mechanical.getImage()))
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
                 .placeholder(R.drawable.ic_loading)
                 .error(R.drawable.ic_error)
                 .apply(RequestOptions.circleCropTransform())
@@ -90,6 +93,7 @@ public class MechanicalAdapter extends RecyclerView.Adapter<MechanicalAdapter.Me
             return results;
         }
 
+        @SuppressWarnings("unchecked")
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
             mechanicalList.clear();
