25e0aa17c0f71333f91ce47bfa67dbf062a62d97
Feluu <rysiak22@gmail.com>
2019-10-30 18:37:57 +0100

    Improvements, changed deprecated functions

--- java/com/feluu/pylife/adapters/WheelsAdapter.java
+++ java/com/feluu/pylife/adapters/WheelsAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,8 +15,10 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.feluu.pylife.R;
+import com.feluu.pylife.WheelsTuneActivity;
 import com.feluu.pylife.models.ListModel;
 
 import java.util.List;
@@ -48,7 +52,8 @@ public class WheelsAdapter extends RecyclerView.Adapter<WheelsAdapter.WheelsView
         holder.textViewDemont.setText(wheels.getThird());
         Glide
                 .with(mCtx)
-                .load(mCtx.getResources().getDrawable(wheels.getImage()))
+                .load(ContextCompat.getDrawable(mCtx, wheels.getImage()))
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
                 .placeholder(R.drawable.ic_loading)
                 .error(R.drawable.ic_error)
                 .apply(RequestOptions.circleCropTransform())
