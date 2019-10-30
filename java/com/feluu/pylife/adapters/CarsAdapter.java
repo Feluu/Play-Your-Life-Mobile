89d4e676f5dd3e8ca0c7c7bc69ad54cb26228c5b
Feluu <rysiak22@gmail.com>
2019-10-30 18:35:21 +0100

    Improvements, changed deprecated functions

--- java/com/feluu/pylife/adapters/CarsAdapter.java
+++ java/com/feluu/pylife/adapters/CarsAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.widget.TextView;
 import android.widget.Filter;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.feluu.pylife.R;
 import com.feluu.pylife.models.ListModel;
@@ -55,7 +57,8 @@ public class CarsAdapter extends RecyclerView.Adapter<CarsAdapter.CarsViewHolder
         holder.textViewReset.setText(cars.getFifth());
         Glide
                 .with(mCtx)
-                .load(mCtx.getResources().getDrawable(cars.getImage()))
+                .load(ContextCompat.getDrawable(mCtx, cars.getImage()))
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
                 .placeholder(R.drawable.ic_loading)
                 .error(R.drawable.ic_error)
                 .apply(RequestOptions.circleCropTransform())
@@ -91,6 +94,7 @@ public class CarsAdapter extends RecyclerView.Adapter<CarsAdapter.CarsViewHolder
             return results;
         }
 
+        @SuppressWarnings("unchecked")
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
             carsList.clear();
