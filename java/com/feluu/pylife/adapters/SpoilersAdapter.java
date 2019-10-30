e2adf208487696b7fa2d456ad3fb22091a2aebb6
Feluu <rysiak22@gmail.com>
2019-10-30 18:37:43 +0100

    Improvements, changed deprecated functions

--- java/com/feluu/pylife/adapters/SpoilersAdapter.java
+++ java/com/feluu/pylife/adapters/SpoilersAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.feluu.pylife.R;
 import com.feluu.pylife.models.ListModel;
@@ -53,7 +55,8 @@ public class SpoilersAdapter extends RecyclerView.Adapter<SpoilersAdapter.Spoile
         holder.textViewDemont.setText(spoilers.getThird());
         Glide
                 .with(mCtx)
-                .load(mCtx.getResources().getDrawable(spoilers.getImage()))
+                .load(ContextCompat.getDrawable(mCtx, spoilers.getImage()))
+                .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
                 .placeholder(R.drawable.ic_loading)
                 .error(R.drawable.ic_error)
                 .apply(RequestOptions.circleCropTransform())
@@ -89,6 +92,7 @@ public class SpoilersAdapter extends RecyclerView.Adapter<SpoilersAdapter.Spoile
             return results;
         }
 
+        @SuppressWarnings("unchecked")
         @Override
         protected void publishResults(CharSequence constraint, FilterResults results) {
             spoilersList.clear();
