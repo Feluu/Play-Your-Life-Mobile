10a980ef4cde1eaa0908c8e4176a043b0d68ccfe
Feluu <rysiak22@gmail.com>
2019-11-22 19:53:19 +0100

    Remove unused imports

--- java/com/feluu/pylife/adapters/WheelsAdapter.java
+++ java/com/feluu/pylife/adapters/WheelsAdapter.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,7 +17,6 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.request.RequestOptions;
 import com.feluu.pylife.R;
-import com.feluu.pylife.WheelsTuneActivity;
 import com.feluu.pylife.models.ListModel;
 
 import java.util.List;
