b12ab52a56240391cff173800984dac003185082
Feluu <rysiak22@gmail.com>
2020-02-23 15:14:51 +0100

    up

--- java/com/feluu/pylife/VisualActivity.java
+++ java/com/feluu/pylife/VisualActivity.java
@@ -108,6 +108,49 @@ public class VisualActivity extends AppCompatActivity {
                 models.add(new ListModel(intToString(R.string.uranus_xflow_wydech_nazwa), intToString(R.string.uranus_xflow_wydech_cena), intToString(R.string.uranus_xflow_wydech_demont), null, null, R.drawable.wydech_xflow_uranus));
                 models.add(new ListModel(intToString(R.string.uranus_alien_wydech_nazwa), intToString(R.string.uranus_alien_wydech_cena), intToString(R.string.uranus_alien_wydech_demont), null, null, R.drawable.wydech_alien_uranus));
             }
+            if (car.equalsIgnoreCase("stratum")) {
+                models.add(new ListModel(intToString(R.string.stratum_xflow_zderzak_przod_nazwa), intToString(R.string.stratum_xflow_zderzak_przod_cena), intToString(R.string.stratum_xflow_zderzak_przod_demont), null, null, R.drawable.przedni_zderzak_xflow_stratum));
+                models.add(new ListModel(intToString(R.string.stratum_alien_zderzak_przod_nazwa), intToString(R.string.stratum_alien_zderzak_przod_cena), intToString(R.string.stratum_alien_zderzak_przod_demont), null, null, R.drawable.przedni_zderzak_alien_stratum));
+                models.add(new ListModel(intToString(R.string.stratum_xflow_zderzak_tyl_nazwa), intToString(R.string.stratum_xflow_zderzak_tyl_cena), intToString(R.string.stratum_xflow_zderzak_tyl_demont), null, null, R.drawable.tylni_zderzak_xflow_stratum));
+                models.add(new ListModel(intToString(R.string.stratum_alien_zderzak_tyl_nazwa), intToString(R.string.stratum_alien_zderzak_tyl_cena), intToString(R.string.stratum_alien_zderzak_tyl_demont), null, null, R.drawable.tylni_zderzak_alien_stratum));
+                models.add(new ListModel(intToString(R.string.stratum_xflow_progi_nazwa), intToString(R.string.stratum_xflow_progi_cena), intToString(R.string.stratum_xflow_progi_demont), null, null, R.drawable.progi_xflow_stratum));
+                models.add(new ListModel(intToString(R.string.stratum_alien_progi_nazwa), intToString(R.string.stratum_alien_progi_cena), intToString(R.string.stratum_alien_progi_demont), null, null, R.drawable.progi_alien_stratum));
+                models.add(new ListModel(intToString(R.string.stratum_xflow_wloty_dach_nazwa), intToString(R.string.stratum_xflow_wloty_dach_cena), intToString(R.string.stratum_xflow_wloty_dach_demont), null, null, R.drawable.wloty_xflow_stratum));
+                models.add(new ListModel(intToString(R.string.stratum_alien_wloty_dach_nazwa), intToString(R.string.stratum_alien_wloty_dach_cena), intToString(R.string.stratum_alien_wloty_dach_demont), null, null, R.drawable.wloty_alien_stratum));
+                models.add(new ListModel(intToString(R.string.stratum_xflow_wydech_nazwa), intToString(R.string.stratum_xflow_wydech_cena), intToString(R.string.stratum_xflow_wydech_demont), null, null, R.drawable.wydech_xflow_stratum));
+                models.add(new ListModel(intToString(R.string.stratum_alien_wydech_nazwa), intToString(R.string.stratum_alien_wydech_cena), intToString(R.string.stratum_alien_wydech_demont), null, null, R.drawable.wydech_alien_stratum));
+            }
+            if (car.equalsIgnoreCase("perennial")) {
+                models.add(new ListModel(intToString(R.string.perennial_przeciwmgielne_nazwa), intToString(R.string.perennial_przeciwmgielne_cena), intToString(R.string.perennial_przeciwmgielne_demont), null, null, R.drawable.przeciwmgielne_perennial));
+                models.add(new ListModel(intToString(R.string.perennial_progi_nazwa), intToString(R.string.perennial_progi_cena), intToString(R.string.perennial_progi_demont), null, null, R.drawable.progi_perennial));
+                models.add(new ListModel(intToString(R.string.perennial_twincylinder_wydech_nazwa), intToString(R.string.perennial_twincylinder_wydech_cena), intToString(R.string.perennial_twincylinder_wydech_demont), null, null, R.drawable.wydech_twincylinder_perennial));
+                models.add(new ListModel(intToString(R.string.perennial_medium_wydech_nazwa), intToString(R.string.perennial_medium_wydech_cena), intToString(R.string.perennial_medium_wydech_demont), null, null, R.drawable.wydech_medium_perennial));
+                models.add(new ListModel(intToString(R.string.perennial_large_wydech_nazwa), intToString(R.string.perennial_large_wydech_cena), intToString(R.string.perennial_large_wydech_demont), null, null, R.drawable.wydech_large_perennial));
+            }
+            if (car.equalsIgnoreCase("premier")) {
+                models.add(new ListModel(intToString(R.string.premier_champ_maska_nazwa), intToString(R.string.premier_champ_maska_cena), intToString(R.string.premier_champ_maska_demont), null, null, R.drawable.maska_champ_premier));
+                models.add(new ListModel(intToString(R.string.premier_fury_maska_nazwa), intToString(R.string.premier_fury_maska_cena), intToString(R.string.premier_fury_maska_demont), null, null, R.drawable.maska_fury_premier));
+                models.add(new ListModel(intToString(R.string.premier_wloty_nazwa), intToString(R.string.premier_wloty_cena), intToString(R.string.premier_wloty_demont), null, null, R.drawable.wloty_premier));
+                models.add(new ListModel(intToString(R.string.premier_twincylinder_wydech_nazwa), intToString(R.string.premier_twincylinder_wydech_cena), intToString(R.string.premier_twincylinder_wydech_demont), null, null, R.drawable.wydech_twincylinder_premier));
+                models.add(new ListModel(intToString(R.string.premier_medium_wydech_nazwa), intToString(R.string.premier_medium_wydech_cena), intToString(R.string.premier_medium_wydech_demont), null, null, R.drawable.wydech_medium_premier));
+            }
+            if (car.equalsIgnoreCase("phoenix")) {
+                models.add(new ListModel(intToString(R.string.phoenix_oval_wentylacja_nazwa), intToString(R.string.phoenix_oval_wentylacja_cena), intToString(R.string.phoenix_oval_wentylacja_demont), null, null, R.drawable.wentylacja_oval_phoenix));
+                models.add(new ListModel(intToString(R.string.phoenix_square_wentylacja_nazwa), intToString(R.string.phoenix_square_wentylacja_cena), intToString(R.string.phoenix_square_wentylacja_demont), null, null, R.drawable.wentylacja_square_phoenix));
+                models.add(new ListModel(intToString(R.string.phoenix_wloty_nazwa), intToString(R.string.phoenix_wloty_cena), intToString(R.string.phoenix_wloty_demont), null, null, R.drawable.wloty_phoenix));
+                models.add(new ListModel(intToString(R.string.phoenix_progi_nazwa), intToString(R.string.phoenix_progi_cena), intToString(R.string.phoenix_progi_demont), null, null, R.drawable.progi_phoenix));
+                models.add(new ListModel(intToString(R.string.phoenix_upsweptc_wydech_nazwa), intToString(R.string.phoenix_upsweptc_wydech_cena), intToString(R.string.phoenix_upsweptc_wydech_demont), null, null, R.drawable.wydechy_upsweptc_phoenix));
+                models.add(new ListModel(intToString(R.string.phoenix_twincylinder_wydech_nazwa), intToString(R.string.phoenix_twincylinder_wydech_cena), intToString(R.string.phoenix_twincylinder_wydech_demont), null, null, R.drawable.wydechy_twincylinder_phoenix));
+                models.add(new ListModel(intToString(R.string.phoenix_large_wydech_nazwa), intToString(R.string.phoenix_large_wydech_cena), intToString(R.string.phoenix_large_wydech_demont), null, null, R.drawable.wydechy_large_phoenix));
+            }
+            if (car.equalsIgnoreCase("zr-350")) {
+                models.add(new ListModel(intToString(R.string.zr350_wloty_nazwa), intToString(R.string.zr350_wloty_cena), intToString(R.string.zr350_wloty_demont), null, null, R.drawable.wloty_zr350));
+                models.add(new ListModel(intToString(R.string.zr350_progi_nazwa), intToString(R.string.zr350_progi_cena), intToString(R.string.zr350_progi_demont), null, null, R.drawable.progi_zr350));
+                models.add(new ListModel(intToString(R.string.zr350_upsweptc_wydech_nazwa), intToString(R.string.zr350_upsweptc_wydech_cena), intToString(R.string.zr350_upsweptc_wydech_demont), null, null, R.drawable.wydechy_upsweptc_zr350));
+                models.add(new ListModel(intToString(R.string.zr350_twincylinder_wydech_nazwa), intToString(R.string.zr350_twincylinder_wydech_cena), intToString(R.string.zr350_twincylinder_wydech_demont), null, null, R.drawable.wydechy_twincylinder_zr350));
+                models.add(new ListModel(intToString(R.string.zr350_large_wydech_nazwa), intToString(R.string.zr350_large_wydech_cena), intToString(R.string.zr350_large_wydech_demont), null, null, R.drawable.wydechy_large_zr350));
+                models.add(new ListModel(intToString(R.string.zr350_medium_wydech_nazwa), intToString(R.string.zr350_medium_wydech_cena), intToString(R.string.zr350_medium_wydech_demont), null, null, R.drawable.wydechy_medium_zr350));
+            }
             WheelsAdapter adapter = new WheelsAdapter(this, models);
             recyclerView.setAdapter(adapter);
         }
